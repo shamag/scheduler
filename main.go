83e4316abfb15133409b0154d21d9693b188138c
Andrey <otbacmhe@gmail.com>
2019-03-30 11:44:17 +0300

    fix

--- main.go
+++ main.go
@@ -6,22 +6,22 @@ import (
 )
 
 type Scheduler struct {
-	tasks []func() (string, error)
-	wg sync.WaitGroup
+	tasks       []func() (string, error)
+	wg          *sync.WaitGroup
 	threadCount int
-	result []string
-	ch chan func() (string, error)
-	errChan chan error
-	semChan chan struct{}
-	resultChan chan string
-	endChannel chan struct{}
+	result      []string
+	ch          chan func() (string, error)
+	errChan     chan error
+	semChan     chan struct{}
+	resultChan  chan string
+	endChannel  chan struct{}
 }
 
-func heavyTask () (string, error) {
+func heavyTask() (string, error) {
 	return "done", nil
 }
 
-func CreateScheduler () Scheduler {
+func CreateScheduler() Scheduler {
 	var wg sync.WaitGroup
 	threadCount := 4
 	semChan := make(chan struct{}, threadCount)
@@ -31,38 +31,40 @@ func CreateScheduler () Scheduler {
 	result := make([]string, 0)
 	tasks := make([]func() (string, error), 0)
 	resultChan := make(chan string)
-	for i:=0;i<10 ;i++  {
+	for i := 0; i < 10; i++ {
 		tasks = append(tasks, heavyTask)
 	}
 
-
 	sched := Scheduler{
-		wg: wg,
-		semChan: semChan,
+		wg:          &wg,
+		semChan:     semChan,
 		threadCount: 4,
-		result: result,
-		errChan: errChan,
-		ch: ch,
-		tasks: tasks,
-		resultChan: resultChan,
-		endChannel: endChan,
+		result:      result,
+		errChan:     errChan,
+		ch:          ch,
+		tasks:       tasks,
+		resultChan:  resultChan,
+		endChannel:  endChan,
 	}
 	return sched
 }
 func (self *Scheduler) Start() {
 	go func() {
-		for i := range(self.tasks) {
+		for i := range self.tasks {
 			self.ch <- self.tasks[i]
 		}
+		close(self.ch)
 	}()
 
 	self.wg.Add(1)
+	fmt.Println("incr wg")
 	println("старт")
 	go func() {
 		for fn := range self.ch {
 			self.semChan <- struct{}{}
 			self.wg.Add(1)
-			go func(fn func() (string, error) ) {
+			fmt.Println("incr wg")
+			go func(fn func() (string, error)) {
 				fmt.Println("Запуск функции")
 				res, err := fn()
 				if err != nil {
@@ -71,33 +73,36 @@ func (self *Scheduler) Start() {
 				self.resultChan <- res
 				<-self.semChan
 				self.wg.Done()
+				fmt.Println("decr wg")
 			}(fn)
 		}
+		fmt.Println("decr wg")
 		self.wg.Done()
 	}()
 	go func() {
 		self.wg.Wait()
 		fmt.Println("завершение")
+
 		self.endChannel <- struct{}{}
 
 	}()
 	fmt.Println("Запуск слушателей")
-	label: for ;;{
+label:
+	for {
 		select {
-			case err:=<-self.errChan:
-				fmt.Println(err)
-			    break label
-			case <- self.endChannel:
-				fmt.Println("end")
-				break label
-			case res:=<-self.resultChan:
-				fmt.Println(res)
-				break label
+		case err := <-self.errChan:
+			fmt.Println(err)
+			break label
+		case <-self.endChannel:
+			fmt.Println("end")
+			break label
+		case res := <-self.resultChan:
+			fmt.Println(res)
 		}
 	}
 }
 
-func main () {
-	scheduler := CreateScheduler ()
+func main() {
+	scheduler := CreateScheduler()
 	scheduler.Start()
 }
