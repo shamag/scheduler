ed82c3caf763f55b1c2644cc7d20f5c53bcf69e6
Andrey <otbacmhe@gmail.com>
2019-03-31 16:43:50 +0300

    small fixes

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"time"
 )
 
+// Определение типа функции, которые будет выполняться планировщиком
 type fnToProcess func() (string, error)
 
 type Scheduler struct {
@@ -15,42 +16,45 @@ type Scheduler struct {
 	threadCount int
 	result      []string
 	ch          chan fnToProcess
-	errChan     chan error
 	semChan     chan struct{}
 	resultChan  chan string
 	endChannel  chan struct{}
 }
 
+// Создание функции которая будет выполняться планировщиком
 func createHeavyTask(i int, isErr bool) fnToProcess {
 	return func() (string, error) {
 
 		if isErr {
 			return fmt.Sprintf("err %d", i), errors.New("errors happened")
 		}
-		time.Sleep(time.Microsecond * 1)
+		time.Sleep(time.Millisecond * 5)
 
 		return fmt.Sprintf("done %d", i), nil
 	}
 
 }
 
-func CreateScheduler() Scheduler {
+func CreateScheduler(threads int) Scheduler {
 	var wg sync.WaitGroup
-	threadCount := 4
-	semChan := make(chan struct{}, threadCount)
+	semChan := make(chan struct{}, threads)
 	endChan := make(chan struct{})
 	ch := make(chan fnToProcess, 0)
-	result := make([]string, 0)
 	resultChan := make(chan string)
 
 	sched := Scheduler{
-		wg:          &wg,
-		semChan:     semChan,
-		threadCount: threadCount,
-		result:      result,
-		ch:          ch,
-		resultChan:  resultChan,
-		endChannel:  endChan,
+		// для ожидания окончания выполнения
+		wg: &wg,
+		// канал для ограничения одновременно выполняющихся заданий
+		semChan: semChan,
+		// количество одновременно выполняющихся заданий
+		threadCount: threads,
+		// канал в котроый складываются задания для выполнения
+		ch: ch,
+		// канал для результатов выполнения заданий
+		resultChan: resultChan,
+		// канал для остановки выполнения
+		endChannel: endChan,
 	}
 	return sched
 }
@@ -60,6 +64,7 @@ func (self *Scheduler) AddTask(fn fnToProcess) {
 }
 func (self *Scheduler) Start() {
 	self.wg.Add(1)
+	// Отправляем а канал self.ch функции
 	go func() {
 		defer func() {
 			close(self.ch)
@@ -75,15 +80,21 @@ func (self *Scheduler) Start() {
 		}
 	}()
 
+	// обработка заданий
 	go func() {
+		// каждое задание в своей горутине
 		for fn := range self.ch {
+			// но одновременно не более чем threadCount
 			self.semChan <- struct{}{}
 			self.wg.Add(1)
 			go func(fn fnToProcess) {
 				defer func() {
+					//освобождаем занятый слот
 					<-self.semChan
 					self.wg.Done()
 				}()
+
+				//
 				for {
 					select {
 					default:
@@ -113,6 +124,7 @@ func (self *Scheduler) Start() {
 	go func() {
 		self.wg.Wait()
 		close(self.resultChan)
+		close(self.semChan)
 	}()
 	for result := range self.resultChan {
 		fmt.Println(result)
@@ -121,9 +133,9 @@ func (self *Scheduler) Start() {
 
 func main() {
 
-	scheduler := CreateScheduler()
+	scheduler := CreateScheduler(10)
 	for i := 0; i < 10000; i++ {
-		scheduler.AddTask(createHeavyTask(i, i == 6000))
+		scheduler.AddTask(createHeavyTask(i, i == 4000))
 	}
 	scheduler.Start()
 
